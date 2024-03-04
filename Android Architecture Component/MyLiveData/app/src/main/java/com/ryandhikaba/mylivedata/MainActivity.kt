6b3a0f21cd2905282185b1045317f6c95d0fef20
ryandhikabaa <ryandhikabintang0@gmail.com>
2024-03-04 13:08:52 +0700

    [AndroidArchitectureComponent] Live Data

--- Android Architecture Component/MyLiveData/app/src/main/java/com/ryandhikaba/mylivedata/MainActivity.kt
+++ Android Architecture Component/MyLiveData/app/src/main/java/com/ryandhikaba/mylivedata/MainActivity.kt
@@ -2,10 +2,29 @@ package com.ryandhikaba.mylivedata
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.ryandhikaba.mylivedata.ViewModel.MainViewModel
+import com.ryandhikaba.mylivedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var liveDataTimerViewModel: MainViewModel
+    private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        liveDataTimerViewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        subscribe()
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long?> { aLong ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, aLong)
+            activityMainBinding.timerTextview.text = newText
+        }
+        liveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver)
     }
 }
