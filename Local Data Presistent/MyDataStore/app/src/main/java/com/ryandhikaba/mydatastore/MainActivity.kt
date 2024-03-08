2a3dd99093ef69b8c2e89095dfd3e2c25b55644f
ryandhikabaa <ryandhikabintang0@gmail.com>
2024-03-08 13:16:38 +0700

    add MyDataStore [DARK MODE]

--- Local Data Presistent/MyDataStore/app/src/main/java/com/ryandhikaba/mydatastore/MainActivity.kt
+++ Local Data Presistent/MyDataStore/app/src/main/java/com/ryandhikaba/mydatastore/MainActivity.kt
@@ -2,10 +2,36 @@ package com.ryandhikaba.mydatastore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProvider
+import com.ryandhikaba.mydatastore.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            val pref = SettingPreferences.getInstance(application.dataStore)
+            val settingViewModel = ViewModelProvider(this@MainActivity, ViewModelFactory(pref)).get(
+                SettingViewModel::class.java
+            )
+            settingViewModel.getThemeSettings().observe(this@MainActivity) { isDarkModeActive: Boolean ->
+                if (isDarkModeActive) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    switchTheme.isChecked = true
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    switchTheme.isChecked = false
+                }
+            }
+
+            switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+                settingViewModel.saveThemeSetting(isChecked)
+            }
+        }
     }
 }
