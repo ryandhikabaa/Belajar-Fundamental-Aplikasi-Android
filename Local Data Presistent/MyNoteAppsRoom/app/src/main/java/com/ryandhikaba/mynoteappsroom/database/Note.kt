888ab9712809298eec2f02768520ea44034a6513
ryandhikabaa <ryandhikabintang0@gmail.com>
2024-03-08 18:15:12 +0700

    add MyNoteApps - ROOM

---
+++ Local Data Presistent/MyNoteAppsRoom/app/src/main/java/com/ryandhikaba/mynoteappsroom/database/Note.kt
@@ -0,0 +1,19 @@
+package com.ryandhikaba.mynoteappsroom.database
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity
+class Note {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    var id: Int = 0,
+    @ColumnInfo(name = "title")
+    var title: String? = null,
+    @ColumnInfo(name = "description")
+    var description: String? = null,
+    @ColumnInfo(name = "date")
+    var date: String? = null
+}
