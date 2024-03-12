fd7b5f06161f03031fa4f070cbafc74b5483161b
ryandhikabaa <ryandhikabintang0@gmail.com>
2024-03-12 09:11:16 +0700

    Next Modul , Background Task dan Scheduler

--- Local Data Presistent/MyNoteAppsRoom/app/src/main/java/com/ryandhikaba/mynoteappsroom/ui/ViewModels/MainViewModel.kt
+++ Local Data Presistent/MyNoteAppsRoom/app/src/main/java/com/ryandhikaba/mynoteappsroom/ui/ViewModels/MainViewModel.kt
@@ -1,4 +1,12 @@
 package com.ryandhikaba.mynoteappsroom.ui.ViewModels
 
-class MainViewModel {
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.ryandhikaba.mynoteappsroom.database.Note
+import com.ryandhikaba.mynoteappsroom.repository.NoteRepository
+
+class MainViewModel(application: Application) : ViewModel() {
+    private val mNoteRepository: NoteRepository = NoteRepository(application)
+    fun getAllNotes(): LiveData<List<Note>> = mNoteRepository.getAllNotes()
 }
--- Local Data Presistent/MyNoteAppsRoom/app/src/main/java/com/ryandhikaba/mynoteappsroom/ui/main/MainActivity.kt
+++ Local Data Presistent/MyNoteAppsRoom/app/src/main/java/com/ryandhikaba/mynoteappsroom/ui/main/MainActivity.kt
@@ -1,11 +1,53 @@
-package com.ryandhikaba.mynoteappsroom
+package com.ryandhikaba.mynoteappsroom.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ryandhikaba.mynoteappsroom.databinding.ActivityMainBinding
+import com.ryandhikaba.mynoteappsroom.ui.ViewModels.MainViewModel
+import com.ryandhikaba.mynoteappsroom.ui.adapter.NoteAdapter
+import com.ryandhikaba.mynoteappsroom.ui.ViewModels.ViewModelFactory
+import com.ryandhikaba.mynoteappsroom.ui.insert.NoteAddUpdateActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var _activityMainBinding: ActivityMainBinding? = null
+    private val binding get() = _activityMainBinding
+    private lateinit var adapter: NoteAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val mainViewModel = obtainViewModel(this@MainActivity)
+        mainViewModel.getAllNotes().observe(this) { noteList ->
+            if (noteList != null) {
+                adapter.setListNotes(noteList)
+            }
+        }
+
+        adapter = NoteAdapter()
+        binding?.rvNotes?.layoutManager = LinearLayoutManager(this)
+        binding?.rvNotes?.setHasFixedSize(true)
+        binding?.rvNotes?.adapter = adapter
+
+        binding?.fabAdd?.setOnClickListener {
+            val intent = Intent(this@MainActivity, NoteAddUpdateActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun obtainViewModel(activity: AppCompatActivity): MainViewModel {
+        val factory = ViewModelFactory.getInstance(activity.application)
+        return ViewModelProvider(activity, factory).get(MainViewModel::class.java)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityMainBinding = null
     }
 }
